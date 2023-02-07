1b93cf86ce030944e855b84902489577bf89258a
evilNotready <bogatov.pasha78@gmail.com>
2023-02-07 16:43:25 +0500

    1 commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Игрок: ${player}`;
+    computerText.textContent = `Компьютер: ${computer}`;
+    resultText.textContent = checkWinner();
+}));
+
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+      case 1:
+        computer = "КАМЕНЬ";
+        break;
+      case 2:
+        computer = "БУМАГА";
+        break;
+      case 3:
+        computer = "НОЖНИЦЫ";
+        break;
+    }
+}
+function checkWinner(){
+    if(player == computer){
+      return "Перестань мысли читать";
+    }
+    else if(computer == "КАМЕНЬ"){
+      return (player == "БУМАГА") ? "Победил все таки..." : "Очередной проигрыш"
+    }
+    else if(computer == "БУМАГА"){
+      return (player == "НОЖНИЦЫ") ? "Надо же.. победил" : "Ты проиграл"
+    }
+    else if(computer == "НОЖНИЦЫ"){
+      return (player == "КАМЕНЬ") ? "Выиграл" : "Проиграл"
+    }
+}
